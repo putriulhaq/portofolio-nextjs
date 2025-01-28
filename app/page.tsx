5854ab8ba3033dce53143afa2e36ba0279bef4e1
putriulhaq <putriulhaq0609@gmail.com>
2025-01-28 22:32:17 +0700

    fix about

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,16 @@ export default function Home() {
         <div className="flex justify-start h-screen bg-gray-100">
           <div className="w-full max-w-3xl mx-auto  px-4 pt-20">
             <h1 className="text-4xl font-bold text-black-600 text-gray-700 dark:text-gray-300">Dhiya&apos;Ulhaq Putri Kinanty</h1>
-            <p className="text-lg mt-2 text-gray-400 dark:text-gray-500"> 
+            {/* <p className="text-lg mt-2 text-gray-400 dark:text-gray-500"> 
               Software Engineer
-            </p>
+            </p> */}
             <p className="text-base mt-4 text-left text-justify text-gray-700 dark:text-gray-300">
-              Hi There, Welcome and thank you for visiting this page!
-              I&apos;m a passionate software engineer dedicated to building innovative and efficient solutions.
-              With experience in full-stack development, I enjoy sharing ideas, collaborating, and creating applications that make a real impact.
-              Feel free to explore and reach out if you have any questions or would like to connect!&nbsp;&mdash;&nbsp;😊
+            Hi there! Welcome, and thank you for visiting this page! 
+              I&apos;am Ulhaq and currently working as a software engineer, mostly doing fullstack development. Honestly, Iam really eager to dive deeper into 
+              backend development that&apos;s where my inrterest lies.
+              I&apos;ve been working with Python, PHP, NodeJs, Javascript and recently I’ve started exploring Go,
+              which has been super exciting. Outside of coding, I’m a big fan of sports—pretty much any kind! 
+              Running, badminton, or just anything active—it’s my way of staying balance, let&apos;s connect!&nbsp;&mdash;&nbsp;😊
             </p>
           <PageBlog />
           </div>
