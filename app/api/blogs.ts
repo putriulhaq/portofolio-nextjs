35acc77344545cfcc94ea755e2da02145b11b4f0
putriulhaq <putriulhaq0609@gmail.com>
2025-06-05 16:33:01 +0700

    add new blogs: Create a PostgreSQL database using Docker

--- app/api/blogs.ts
+++ app/api/blogs.ts
@@ -1,12 +1,19 @@
 // data/blogs.ts
 export const blogs = [
+  // {
+  //   slug: "first-blog",
+  //   title: "First Blog",
+  //   description:
+  //     "Untuk membuat file slug pada blog, biasanya digunakan untuk membuat URL yang ramah SEO danmudah diingat. Di bawah ini adalah contoh untuk membuat slug pada blog yang terdiri dari artikel. Biasanya, slug ini akan ditulis dalam format URL-friendly dengan kata-kata yang dipisahkan oleh tanda hubung (-).",
+  //   date: "January 21, 2025",
+  //   link: "/blog/first-blog",
+  // },
   {
-    slug: "first-blog",
-    title: "First Blog",
-    description:
-      "Untuk membuat file slug pada blog, biasanya digunakan untuk membuat URL yang ramah SEO danmudah diingat. Di bawah ini adalah contoh untuk membuat slug pada blog yang terdiri dari artikel. Biasanya, slug ini akan ditulis dalam format URL-friendly dengan kata-kata yang dipisahkan oleh tanda hubung (-).",
-    date: "January 21, 2025",
-    link: "/blog/first-blog",
+    slug: "",
+    title: "Create a PostgreSQL database using Docker",
+    description: "",
+    date: "June 05, 2025",
+    link: "https://ulhaq.notion.site/Initiate-Postgresql-with-Docker-209065fd351080c78fa6d1f5269209a2",
   },
   {
     slug: "",
@@ -15,11 +22,11 @@ export const blogs = [
     date: "June 18, 2024",
     link: "https://ulhaq.notion.site/Elasticsearch-and-Kibana-with-Docker-c74cd6aaf8584472afcb722c5b0fe95b?pvs=4",
   },
-  // {
-  //   slug: "",
-  //   title: "Elasticsearch and Kibana with Docker",
-  //   description: "",
-  //   date: "June 18, 2024",
-  //   link: "https://ulhaq.notion.site/Elasticsearch-and-Kibana-with-Docker-c74cd6aaf8584472afcb722c5b0fe95b?pvs=4",
-  // },
+  {
+    slug: "",
+    title: "Experiment with Optical Character Recognition (OCR)",
+    description: "",
+    date: "June 13, 2024",
+    link: "https://ulhaq.notion.site/Elasticsearch-and-Kibana-with-Docker-c74cd6aaf8584472afcb722c5b0fe95b?pvs=4",
+  },
 ];
